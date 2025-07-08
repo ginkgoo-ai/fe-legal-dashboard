ef24260626b4678b3cbdceee40fa76e519ec11a8
gengjian1203 <gengjian1203@foxmail.com>
2025-07-08 14:24:07 +0800

    feat(logoutTigger): clear extensions tokens and user info when logout

--- components/logoutTigger/index.tsx
+++ components/logoutTigger/index.tsx
@@ -3,6 +3,10 @@ import { logout } from '@/service/api';
 export default function LogoutTrigger({ children }: { children: React.ReactNode }) {
   const handleLogout = () => {
     logout();
+
+    window.postMessage({
+      type: 'ginkgoo-page-background-logout',
+    });
   };
 
   return <div onClick={handleLogout}>{children}</div>;
