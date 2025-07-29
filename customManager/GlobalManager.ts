34121021f78288827367e685d60312d44b57bc21
gengjian1203 <gengjian1203@foxmail.com>
2025-07-29 17:47:23 +0800

    chore(GlobalManager): update extension version and url

--- customManager/GlobalManager.ts
+++ customManager/GlobalManager.ts
@@ -4,9 +4,9 @@ class GlobalManager {
 
   siteName = 'Xeni';
   siteDescription = 'Xeni.Legal';
-  versionExtension = '0.2.4';
+  versionExtension = '0.2.5';
   urlInstallExtension =
-    'https://github.com/ginkgoo-ai/fe-chrome-extensions/releases/download/v0.2.4/xeni-pilot_20250729074451.zip';
+    'https://github.com/ginkgoo-ai/fe-chrome-extensions/releases/download/v0.2.5/xeni-pilot_20250729094451.zip';
 
   static getInstance() {
     if (!this.instance) {
